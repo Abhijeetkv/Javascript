8d445a85c099ea9f601f7233104b29069eea625c
Abhijeet kumar <142344752+Abhijeetkv@users.noreply.github.com>
2024-02-01 12:09:58 +0000

    done

---
+++ JsBasics/operation.js
@@ -0,0 +1,31 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-4);
+// console.log( 2*2 );
+// console.log( 2**3);
+// console.log(7/4);
+// console.log(5%2);
+
+let str1 = "hello"
+let str2 = "  abhijeet"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log( "1" + 2);
+console.log( 1 + "2");
+console.log( "1" + "2");
+console.log( "1" + 2 + 2);
+console.log(2 + 2 + "1");
+
+
+let n1, n2, n3
+
+n1 = n2 = n3 = 2 + 2
+
+let gameCounter = 100 
+gameCounter++;
+console.log(gameCounter);
