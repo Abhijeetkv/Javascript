927f25a2191c569cf054a35a06e74d0984c7f400
Abhijeet kumar <142344752+Abhijeetkv@users.noreply.github.com>
2024-02-01 10:57:57 +0000

    done

---
+++ JsBasics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3) //we are using node js not browser
+
+// String => ""
+// Number
+// BigInt
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// Symbol => unique
+
+
+//object
+
+let age = 14
+let isLoggedIn = false 
+let state;
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
