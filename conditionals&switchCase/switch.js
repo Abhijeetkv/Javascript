e025125f5123f6b3c314a776a5a93deb64245289
Abhijeet kumar <142344752+Abhijeetkv@users.noreply.github.com>
2024-02-01 11:53:23 +0000

    done

---
+++ conditionals&switchCase/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// for duplicating the code use  ctrl + alt + downarrow
+const month = 2
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+        
+    case 3:
+         console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    default:
+        console.log("invalaid month");
+        break;
+}
